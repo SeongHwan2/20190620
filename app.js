387b9d17ca9f53117da3cd66cbd97e07db292dde
ysh <saviora7@hanmail.net>
2019-06-20 12:03:10 +0900

    글수정

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
+var http = require('http');
+var hostname = '127.0.0.1';
+var server = http.createServer(function(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(80, function() {
+  console.log('Server running~');
 });
